0f45d01a860bbceea419e3620f6179a9a88b92e9
Anna Boichuk <anna.boichuk.o@gmail.com>
2024-02-12 18:18:22 +0200

    added search bar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import { Header } from "./Header/Header";
+import { SearchBar } from "./SearchBar/SearchBar";
 
 
 export const App = () => {
   return (
     <div>
 <Header/>
+<SearchBar/>
     </div>
   );
 };
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,11 @@
+import scss from './SearchBar.scss'
+
+export const SearchBar = () => {
+    return(
+       <form >
+        <label htmlFor=""></label>
+        <button></button>
+        <input id='country-name' name='searchCountryName' type="text" placeholder='Search for a country...' />
+       </form>
+    )
+}
