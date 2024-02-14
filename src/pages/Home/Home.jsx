777c650737bdeebc1f16289c052079b127d098df
Anna Boichuk <anna.boichuk.o@gmail.com>
2024-02-14 17:50:07 +0200

    added pagination

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,6 +9,9 @@ import { getCountries } from '../../redux/selectors';
 import { fetchAllCountries } from '../../redux/operations';
 import { useDispatch, useSelector } from 'react-redux';
 import { setCountries } from '../../redux/countriesSlice';
+import { setPagination } from '../../redux/paginationSlice';
+import ReactPaginate from 'react-paginate';
+import { Loader } from 'components/Loader/Loader';
 
 const Home = () => {
 
@@ -19,35 +22,53 @@ const dispatch = useDispatch()
 const countries = useSelector(getCountries)
 
 
+// pagination
+const [itemOffset, setItemOffset] = useState(0);
+const endOffset = itemOffset + 8;
+const currentItems = countries.slice(itemOffset, endOffset);
+const pageCount = Math.ceil(countries.length / 8);
+
+const handlePageClick = (event) => {
+    const newOffset = (event.selected * 8) % countries.length
+    setItemOffset(newOffset);
+    dispatch(setPagination([]))
+  };
+
 useEffect(()=>{
     const fetchCountries = async() =>{
-    try{
-        setIsLoading(true);
-        const response = await fetchAllCountries()
-        if(countries.length === 0){
-            dispatch(setCountries(response))
-        }
+try{
+    const response = await fetchAllCountries()
+    if(countries.length === 0){
+        dispatch(setCountries(response))
     }
-    catch(error){
-        setApiError(true);
-                Notiflix.Notify.failure(
-          `Oops! Something went wrong! Error ${apiError} Try reloading the page!`
-        );
-    }
-    finally {
-        setIsLoading(false);
-
-      }
 }
-fetchCountries()
+catch (error) {
+    setApiError(true);
+    Notiflix.Notify.failure(
+      `Oops! Something went wrong! Error ${apiError} Try reloading the page!`
+    );
+  } finally {
+    setIsLoading(false);
+  }
 
-}, [ apiError, countries, isLoading])
-// const countries = useSelector(getCountries)
-// console.log(countries);
+    }
+    fetchCountries()
+})
+
+
     return(
         <div className={scss.container}>
+            {isLoading && <Loader/>}
             <SearchBar/>
-            <CountryList data = {countries}/>
+             <CountryList data={currentItems}/>
+            <ReactPaginate 
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={pageCount}
+        previousLabel="< previous"
+        renderOnZeroPageCount={null}/>
         </div>
     )
 }
