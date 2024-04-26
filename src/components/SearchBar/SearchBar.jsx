7a5cf3a64dc65e1f162324217a899d8531755567
Anna Boichuk <anna.boichuk.o@gmail.com>
2024-04-26 19:50:05 +0300

    adding dark styles for search

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,8 +1,14 @@
 import scss from './SearchBar.module.scss'
 import {ReactComponent as Search} from '../../images/icons/search.svg'
+import { useSelector } from 'react-redux'
+import { getTheme } from '../../redux/selectors'
+import clsx from 'clsx'
 export const SearchBar = ({handleSubmit, refQuery}) => {
+    const theme = useSelector(getTheme)
     return(
-       <form className={scss.form} onSubmit={handleSubmit}>
+       <form className={clsx(scss.form, {
+        [scss.dark]:theme
+       } )} onSubmit={handleSubmit}>
         <label htmlFor="country-name"></label>
         <button className={scss['btn-search']} type='submit'><Search/></button>
         <input ref={refQuery} className={scss.input} id='country-name' name='searchCountryName' type="text" placeholder='Search for a country...' />
