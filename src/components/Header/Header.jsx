80f6c440887705828081f18dd3d096f24b1fa115
Anna Boichuk <anna.boichuk.o@gmail.com>
2024-04-26 21:00:35 +0300

    added redux-persist for saving theme

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,45 @@
 import { useDispatch, useSelector } from 'react-redux';
 import scss from './Header.module.scss';
-import {  NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { setTheme } from '../../redux/themeSlice';
 import { getTheme } from '../../redux/selectors';
 import clsx from 'clsx';
 export const Header = () => {
-const theme = useSelector(getTheme)
-const dispatch = useDispatch()
-  const handleThemeChange =theme=> dispatch(setTheme(theme))
+  const theme = useSelector(getTheme);
+  const dispatch = useDispatch();
+  const handleThemeChange = theme => dispatch(setTheme(theme));
 
   return (
-    <header className={clsx(scss.header, {
-      [scss.dark]:theme
-  })}>
-      <NavLink className={scss.title} to={`/`}>Where in the world?</NavLink>
+    <header
+      className={clsx(scss.header, {
+        [scss.dark]: theme,
+      })}
+    >
+      <NavLink className={scss.title} to={`/`}>
+        Where in the world?
+      </NavLink>
       <div className={scss['theme-container']}>
         {' '}
-        <input className={scss['theme-input-icon']}  type="checkbox" id='theme' onChange={()=>handleThemeChange(!theme)}/>
-        <label className={scss.theme}  htmlFor="theme">Dark Mode</label>
+        {theme ? (
+          <input
+            checked
+            className={scss['theme-input-icon']}
+            type="checkbox"
+            id="theme"
+            onChange={() => handleThemeChange(!theme)}
+          />
+        ) : (
+          <input
+          checked={false}
+            className={scss['theme-input-icon']}
+            type="checkbox"
+            id="theme"
+            onChange={() => handleThemeChange(!theme)}
+          />
+        )}
+        <label className={scss.theme} htmlFor="theme">
+          Dark Mode
+        </label>
       </div>
     </header>
   );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,54 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { countriesReducer } from './countriesSlice';
 import { paginationReducer } from './paginationSlice';
 import { filterByRegionReducer } from './filterRegionSlice';
 import { borderCountriesReducer } from './borderCountriesSlice';
 import { themeReducer } from './themeSlice';
 
-export const store = configureStore({
-  reducer: 
- {countries: countriesReducer,
-pagination: paginationReducer,
-filterByRegion: filterByRegionReducer,
-borderCountries:borderCountriesReducer,
-theme: themeReducer
-}
+// export const store = configureStore({
+//   reducer: 
+//  {countries: countriesReducer,
+// pagination: paginationReducer,
+// filterByRegion: filterByRegionReducer,
+// borderCountries:borderCountriesReducer,
+// theme: themeReducer
+// }
+// });
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const authConfig = {
+  key: 'theme',
+  storage,
+};
+
+export const rootReducer = combineReducers({
+
+  countries: countriesReducer,
+  pagination: paginationReducer,
+  filterByRegion: filterByRegionReducer,
+  borderCountries:borderCountriesReducer,
+  theme: persistReducer(authConfig, themeReducer)
 });
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
