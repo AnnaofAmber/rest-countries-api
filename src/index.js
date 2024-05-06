d4423c5836c455231671e66bf245d6512b10762f
Anna Boichuk <anna.boichuk.o@gmail.com>
2024-05-06 18:47:21 +0300

    changing readme

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
 <Provider store={store}> 
 
-<BrowserRouter basename='/rest-countries-api/'>
+<BrowserRouter basename='/rest-countries-api'>
 <PersistGate persistor={persistor}>
     <App />
     </PersistGate>
